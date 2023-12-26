ca6d0e4a88c137a06718d0ea2bd64bcff381c187
savithaprakash289 <136881941+savithaprakash289@users.noreply.github.com>
2023-12-26 14:42:15 +0530

    Update app.py

--- app.py
+++ app.py
@@ -14,14 +14,15 @@ mysql = MySQL(app)
 def index():
     if request.method == "POST":
         full_name = request.form["full_name"]
-        mobile_no = request.form["mobile_no"]
+       father_name = request.form["father_name"]
+	phone = request.form["phone "]
         age = request.form["age"]
         address = request.form["address"]
         prescription = request.form["prescription"]
 		
         cur = mysql.connection.cursor()
 
-        cur.execute( "INSERT INTO customers (full_name,mobile_no, age, address, prescription) VALUES(%s, %s, %s, %s, %s)",(full_name,mobile_no, age, address, prescription))
+        cur.execute( "INSERT INTO customers (full_name,father_name,phone, age, address, prescription) VALUES(%s, %s, %s, %s, %s)",(full_name,father_name,phone, age, address, prescription))
         mysql.connection.commit()
         cur.close()
         return "success"
