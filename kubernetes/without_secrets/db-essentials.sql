e974e4470aecac82d0f0ef8677c4c40500673d32
savithaprakash289 <136881941+savithaprakash289@users.noreply.github.com>
2024-01-04 21:32:45 +0530

    Update db-essentials.sql

--- kubernetes/without_secrets/db-essentials.sql
+++ kubernetes/without_secrets/db-essentials.sql
@@ -6,7 +6,6 @@ DROP TABLE IF EXISTS patients;
 
 CREATE TABLE patients (
   full_name  VARCHAR(255) DEFAULT NULL,
-  father_name  VARCHAR(255) DEFAULT NULL,
   phone      VARCHAR(255) DEFAULT NULL,
   address    VARCHAR(255) DEFAULT NULL,
   age  VARCHAR(255) DEFAULT NULL,
@@ -18,10 +17,9 @@ CREATE TABLE patients (
 );
 
 DELIMITER //
-CREATE PROCEDURE create_and_return(IN full_name VARCHAR(255), IN father_name VARCHAR(255),
-                  IN phone VARCHAR(255), IN  age VARCHAR(255),IN address VARCHAR(255), IN gender VARCHAR(255), IN prescription VARCHAR(255))
+CREATE PROCEDURE create_and_return(IN full_name VARCHAR(255),  IN phone VARCHAR(255), IN  age VARCHAR(255),IN address VARCHAR(255), IN gender VARCHAR(255), IN prescription VARCHAR(255))
 BEGIN
-  INSERT INTO patients (full_name, father_name, phone, address, age, gender, prescription) VALUES (full_name, father_name,  phone, address, age, gender, prescription);
+  INSERT INTO patients (full_name, phone, address, age, gender, prescription) VALUES (full_name, phone, address, age, gender, prescription);
 
   SET @PATIENT_AGE= LAST_INSERT_AGE();
 
